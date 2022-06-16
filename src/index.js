42da1dbdb4d037f628396b630cb48b215cc13823
Jackline Jebet <jebetjackline2016@gmail.com>
2022-06-16 23:27:44 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener("onload",function(){
+    fetchDogImages()
+})
+function fetchDogImages(){
+    fetch ("https://dog.ceo/api/breeds/image/random/4")
+    .then(response=>response.json())
+    .then(data=>console.log(data))
+} 
